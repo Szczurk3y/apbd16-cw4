5e526aa980f64de18f41350c141348552da1265f
s24196 <s24196@pjwstk.edu.pl>
2025-04-13 16:07:06 +0200

    Add

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -5,6 +5,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// 1. Dodajemy kontrolley do buildingu
+builder.Services.AddControllers();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -16,6 +18,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-
+// 2. Mapujemy końcówki kontrollerów
+app.MapControllers();
 
 app.Run();
