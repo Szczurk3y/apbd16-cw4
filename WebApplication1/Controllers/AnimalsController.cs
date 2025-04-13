aead96e88517f313cf0e91d98126a34b69fe2643
s24196 <s24196@pjwstk.edu.pl>
2025-04-13 16:02:10 +0200

    Add

---
+++ WebApplication1/Controllers/AnimalsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    // app/animals => [controller] = Animals
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AnimalsController : ControllerBase
+    {
+        // GET api/animals
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Ok("Hello world!");
+        }
+    }
+}
+
